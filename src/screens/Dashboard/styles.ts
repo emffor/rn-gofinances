159e3a3d6f124a86b58d03f47b9d223b5218a34a
emffor <98027423+emffor@users.noreply.github.com>
2022-03-10 13:23:56 -0300

    fix: Criando estilos globais

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,16 @@
 import styled from 'styled-components/native';
+import theme from '../../global/styles/theme';
 
 export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: #121015;
+    /* background-color: ${(props) => props.theme.colors.primary} */
+    background-color: ${({theme}) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
     font-size: 50px;
     font-weight: bold;
-    color: orange;
+    color: ${({ theme }) => theme.colors.title};
 `;
