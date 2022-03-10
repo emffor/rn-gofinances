73ede556c3bc2d99e6390b69ab4c4fd0624f92e5
emffor <98027423+emffor@users.noreply.github.com>
2022-03-10 11:23:36 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar  } from 'react-native';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+
