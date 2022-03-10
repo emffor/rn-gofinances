21496934b3fc25279cd1659aa7cf94229961f3dc
emffor <98027423+emffor@users.noreply.github.com>
2022-03-10 12:16:51 -0300

    fix: iniciando styled components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar  } from 'react-native';
 import { Dashboard } from './src/screens/Dashboard';
+
 
 export default function App() {
   return (
     <>
-      <Dashboard />
+            <Dashboard />
     </>
   );
 }
