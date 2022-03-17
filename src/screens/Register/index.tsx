3b75ecbf69a568c94b1e3ba455bbcb36da29c22c
emffor <98027423+emffor@users.noreply.github.com>
2022-03-17 08:25:37 -0300

    fix: Salvando novas transações

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { useForm } from "react-hook-form";
+import { useNavigation } from '@react-navigation/native';
 
 import { 
   Modal, 
@@ -14,7 +15,9 @@ import { Button } from '../../components/Form/Button';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { InputForm } from '../../components/Form/InputForm';
+import uuid from 'react-native-uuid';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+
 
 import {
     Container,
@@ -32,6 +35,10 @@ import { yupResolver } from '@hookform/resolvers/yup'
 interface FormData {
   name: string;
   amount: string;
+}
+
+type NavigationProps = {
+  navigate: (screen:string) => void;
 }
 
 //Nome é obrigatório
@@ -61,6 +68,9 @@ export function Register(){
       name: 'Categoria',
   });
 
+  //tipado para evitar erro de tipagem
+  const navigation = useNavigation<NavigationProps>();
+
   //estado dos inputs
   const [name, setName] = useState('');
   const [amount, setAmount] = useState('');
@@ -83,6 +93,7 @@ export function Register(){
   const {
     control, 
     handleSubmit,
+    reset,
     formState: { errors } //desestruturando error do FormState
   } = useForm({
     //força q o valor do formulário siga um padrão.
@@ -97,11 +108,14 @@ export function Register(){
      if (category.key === 'category')
       return Alert.alert('Selecione a categoria');
     
+    //id é gerado pelo back - mas não como ñ tem.... usa o uidd
     const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name, 
       amount: form.amount,
       transactionType,
-      category: category.key
+      category: category.key,
+      date: new Date(),
     }
     
     //1- passa uma chave / 2- passa a coleção @nomedoapp / 3- passa a chave pro AsyncStorage no setItem(chave, passa objeto convertido pra texto com JSON.stringify
@@ -121,31 +135,20 @@ export function Register(){
 
       await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted)); 
 
+      reset();
+      setTransactionType('')
+      setCategory({
+        key: 'category',
+        name: 'Categoria'
+      });
+
+      navigation.navigate('Listagem');
+
     } catch (error) {
       console.log(error);
       Alert.alert("Não foi possível salvar");
     }
   }
-
-  //useEffect- não consigo dizer q useEffect é uma função async então cria uma funçãozinha ex: loadData. getItem(da onde pegar as transações) por isso chave é importando para resgatar e pra pegar.
-
-  //JSON.parse(data!) faz o contrario de JSON.stringify e o data! - a ! é um recurso do TypeScript que basicamente diz que pode confiar que sempre vai ter alguma nesse data
-
-  useEffect(() => {
-     async function loadData(){
-      const data = await AsyncStorage.getItem(dataKey);
-      console.log(JSON.parse(data!)); 
-     }
-
-     loadData()
-
-
-     //FUNÇÃO PARA LIMPAR UMA COLEÇÃO DO ASYNC STORAGE
-     /* async function removeAll() {
-       await AsyncStorage.removeItem(dataKey);
-     } 
-     removeAll(); */
-  },[]);
 
   //PODE USAR ESSA DE BAIXO
   // function handleRegister() {
