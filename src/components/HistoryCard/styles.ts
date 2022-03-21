022948c09959d5630599b34b4eff32364561d2f4
emffor <98027423+emffor@users.noreply.github.com>
2022-03-21 20:15:01 -0300

    Criando a interface de gráfico
    Estilizando interface de gráfico

--- src/components/HistoryCard/styles.ts
+++ src/components/HistoryCard/styles.ts
@@ -1,3 +1,4 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 interface ContainerProps {
@@ -5,13 +6,29 @@ interface ContainerProps {
 }
 
 export const Container = styled.View<ContainerProps>`
+    width: 100%;
 
+    background-color: ${({ theme }) => theme.colors.shape};
+
+    flex-direction: row;
+    justify-content: space-between;
+
+    padding: 13px 24px;
+
+    border-radius: 5px;
+    border-left-width: 5px;
+    /* passado pela cor no index */
+    border-left-color: ${({ color }) => color}; 
+
+    margin-bottom: 8px;
 `;
 
 export const Title = styled.Text`
-
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(15)}px;
 `;
 
 export const Amount = styled.Text`
-
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(15)}px;
 `;
